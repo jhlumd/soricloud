92a1b24f15106ac3f69c81034acce09f695b2a75
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-05 11:31:06 -0800

    Complete testing on window session utils signup, login, logout

--- frontend/soricloud.jsx
+++ frontend/soricloud.jsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
+// import { signup, login, logout} from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     const store = configureStore();
+
+    // window.signup = signup;
+    // window.login = login;
+    // window.logout = logout;
 
     ReactDOM.render(<Root store={store} />, root);
 });
