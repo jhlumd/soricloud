c2c43805c9346fa3d889f0a9aa56ec1d82e2bd8a
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-05 14:02:39 -0800

    Finish testing getState and dispatch on window

--- frontend/soricloud.jsx
+++ frontend/soricloud.jsx
@@ -4,6 +4,11 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 // import { signup, login, logout} from './util/session_api_util';
+// import {
+//     receiveCurrentUser,
+//     logoutCurrentUser,
+//     receiveErrors
+// } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
@@ -12,6 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // window.signup = signup;
     // window.login = login;
     // window.logout = logout;
+    
+    // window.receiveCurrentUser = receiveCurrentUser;
+    // window.logoutCurrentUser = logoutCurrentUser;
+    // window.receiveErrors = receiveErrors;
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
 
     ReactDOM.render(<Root store={store} />, root);
 });
