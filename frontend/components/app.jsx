6a9b8a1cef7dc8024a0baf109776c3a0d8f3e53d
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-05 09:45:05 -0800

    Add basic store, app, root to render simple heading at localhost

---
+++ frontend/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// import TrackIndexContainer from './tracks/track_index_container';
+// import TrackShowContainer from './tracks/track_show_container';
+// import EditTrackFormContainer from './tracks/edit_track_form_container';
+
+const App = () => (
+    <div>
+        <h1>MY FULLSTACK APP!!!</h1>
+        {/* <Switch>
+            <Route exact path="/" component={TrackIndexContainer} />
+            <Route exact path="/tracks/:trackId" component={TrackShowContainer} />
+            <Route path="/tracks/:trackId/edit" component={EditTrackFormContainer} />
+        </Switch> */}
+    </div>
+);
+
+export default App;
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import EntitiesReducer from './entities_reducer';
+
+const RootReducer = combineReducers({
+
+});
+
+export default RootReducer;
