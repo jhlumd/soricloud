870115cb38ff02a004d4c16892eaabf250e65e4d
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-05 17:47:29 -0800

    Before making separate signin, login forms to connect with respective containers

--- frontend/components/session_form/login_form_container.jsx
+++ frontend/components/session_form/login_form_container.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import { login } from '../../actions/session_actions';
 import { openModal, closeModal } from '../../actions/modal_actions';
@@ -10,13 +9,18 @@ const mapStateToProps = ({ errors }) => {
     return {
         errors: errors.session,
         formType: 'login',
-        navLink: <Link to="/signup">sign up instead</Link>,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
         processForm: (user) => dispatch(login(user)),
+        otherForm: (
+            <button onClick={() => dispatch(openModal('signup'))}>
+                Signup
+      </button>
+        ),
+        closeModal: () => dispatch(closeModal())
     };
 };
 
