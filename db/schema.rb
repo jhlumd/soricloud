5f77b04be749b9dc8c2125bf6523d01fd61c4c0c
Jaehyuk Lee <jhlumd@gmail.com>
2019-12-24 23:30:32 -0800

    Add Track model and delete image_url column from Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_174914) do
+ActiveRecord::Schema.define(version: 2019_12_25_071958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title", null: false
+    t.boolean "private", default: false
+    t.integer "user_id", null: false
+    t.string "genre"
+    t.string "description"
+    t.string "tags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre"], name: "index_tracks_on_genre"
+    t.index ["user_id"], name: "index_tracks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
-    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
