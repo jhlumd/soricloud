4325cc0ea1db2bca76e5eb742ebea960e56ec820
Jaehyuk Lee <jhlumd@gmail.com>
2019-12-25 17:17:13 -0800

    Define all routes for tracks controller

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -13,4 +13,35 @@ class Api::TracksController < ApplicationController
       render json: ['No track found'], status: 422
     end
   end
+
+  def create
+    @track = Track.new(track_params)
+    if @track.save
+      render "api/tracks/show"
+    else
+      render json: @track.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @track = Track.find_by(id: params[:track][:id])
+
+    if @track.update(track_params)
+      render "api/tracks/show"
+    else
+      render json: @track.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    track = Track.find_by(id: params[:id])
+    track.destroy
+    render json: { id: params[:id] }
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:title, :private, :user_id, :genre, :description, :tags, :photo, :audio_file)
+  end
 end
