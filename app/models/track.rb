4e72d4a03a8f754ec23f9af293f3f5e36bb56c32
Jaehyuk Lee <jhlumd@gmail.com>
2019-12-26 00:34:45 -0800

    Add validation for audio file attached in Track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -15,6 +15,7 @@
 
 class Track < ApplicationRecord
   validates :title, presence: true
+  validate :ensure_audio_file
 
   has_one_attached :photo
   has_one_attached :audio_file
@@ -23,4 +24,10 @@ class Track < ApplicationRecord
     primary_key: :id,
     foreign_key: :user_id,
     class_name: 'User'
+
+  def ensure_audio_file
+    unless self.audio_file.attached?
+      errors[:audio_file] << "must be attached"
+    end
+  end
 end
