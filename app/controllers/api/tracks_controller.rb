ab58e508ae195ad0d838c1ed9dc01797fa6a9eb0
Jaehyuk Lee <jhlumd@gmail.com>
2019-12-25 16:58:58 -0800

    Add index to tracks controller

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -1,4 +1,9 @@
 class Api::TracksController < ApplicationController
+  def index
+    @tracks = Track.all.with_attached_photo.with_attached_audio_file.includes(:user)
+    render "api/tracks/index"
+  end
+
   def show
     @track = Track.find_by(id: params[:id])
 
