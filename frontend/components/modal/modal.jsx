93f11b43f6fafc27491d5cb2092a3b4657b7c69c
Jaehyuk Lee <jhlumd@gmail.com>
2020-01-03 14:26:02 -0800

    Change modal code styling

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -5,45 +5,48 @@ import { closeModal } from '../../actions/modal_actions';
 import LoginInputFormContainer from '../session_form/login_input_form_container';
 
 class Modal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            modalReady: false
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalReady: false
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.props.modal && !this.state.modalReady) {
+      setTimeout(() => this.setState({ modalReady: true }), 200);
+    } else if (!this.props.modal && this.state.modalReady) {
+      this.setState({ modalReady: false });
     }
+  }
 
-    componentDidUpdate() {
-        if (this.props.modal && !this.state.modalReady) {
-            setTimeout(() => this.setState({ modalReady: true }), 200);
-        } else if (!this.props.modal && this.state.modalReady) {
-            this.setState({ modalReady: false });
-        }
+  render() {
+    if (!this.props.modal) {
+      return null;
     }
-
-
-    render() {
-        if (!this.props.modal) {
-            return null;
-        }
-        let component;
-        switch (this.props.modal) {
-            case 'loginInput':
-                component = <LoginInputFormContainer />;
-                break;
-            default:
-                return null;
-        }
-        return (
-            <div className="modal-background" onClick={() => this.props.closeModal()}>
-                <div className="modal-positioner">
-                    <div className={`modal-child-start ${this.state.modalReady ? "modal-child" : ""}`} onClick={e => e.stopPropagation()}>
-                        {component}
-                    </div>
-                </div>
-
-            </div>
-        );
+    let component;
+    switch (this.props.modal) {
+      case "loginInput":
+        component = <LoginInputFormContainer />;
+        break;
+      default:
+        return null;
     }
+    return (
+      <div className="modal-background" onClick={() => this.props.closeModal()}>
+        <div className="modal-positioner">
+          <div
+            className={`modal-child-start ${
+              this.state.modalReady ? "modal-child" : ""
+            }`}
+            onClick={e => e.stopPropagation()}
+          >
+            {component}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mstp = (state) => {
