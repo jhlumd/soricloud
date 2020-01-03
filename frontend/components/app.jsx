b0dd6a1bcb0baaea036655d2d180fa44cd8abfd9
Jaehyuk Lee <jhlumd@gmail.com>
2020-01-03 14:14:19 -0800

    on

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,7 +4,6 @@ import { Route, Redirect, Switch, Link } from "react-router-dom";
 import Modal from './modal/modal';
 import SplashContainer from '../components/splash/splash_container';
 import TrackIndex from '../components/tracks/track_index';
-
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
