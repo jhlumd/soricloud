d37e24403018f6432f36299d6ee0ea0a254cc17b
Jaehyuk Lee <jhlumd@gmail.com>
2020-01-10 22:33:42 -0800

    Change trackupload to functional

--- frontend/components/tracks/track_upload.jsx
+++ frontend/components/tracks/track_upload.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class TrackUpload extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
+export default function TrackUpload(props) {
+  return (
+    <div>
+      
+    </div>
+  )
 }
