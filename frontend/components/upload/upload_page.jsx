006e232f74e0991964a12df8d5be8b673813875c
Jaehyuk Lee <jhlumd@gmail.com>
2020-01-16 16:33:39 -0800

    Add upload page markup

--- frontend/components/upload/upload_page.jsx
+++ frontend/components/upload/upload_page.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import MainForm from './main_form';
 
 export default function UploadPage(props) {
+  const handleDrag = e => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      
+    <div className="upload-page" onDragOver={handleDrag}>
+      <div className="upload-page-nav">
+        <div className="upload-page-nav-links">
+          <div className="upload-page-nav-link">
+            Upload
+          </div>
+          <div className="track-link-placeholder"></div>
+        </div>
+        <button id="for-creators" className="upload-page-nav-button">
+          For Creators on SoriCloud
+        </button>
+      </div>
+      <MainForm
+        userId={props.userId}
+        uploadTrack={props.uploadTrack}
+        openModal={props.openModal}
+        history={props.history}
+      />
     </div>
-  )
+  );
 }
