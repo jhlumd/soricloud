8dc0e05c4e72b8284ece3e8c9b4effaab84b9bbb
Jaehyuk Lee <jhlumd@gmail.com>
2020-01-18 14:19:23 -0800

    Add abbrev to app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,23 +4,23 @@ import { Route, Switch } from "react-router-dom";
 import Modal from "./modal/modal";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
-import SplashContainer from "../components/splash/splash_container";
+import SplashC from "../components/splash/splash_container";
 import TrackIndex from "../components/tracks/track_index";
 import UploadPage from "../components/upload/upload_page";
-// import TrackPlayerContainer from "../components/track_player/track_player_container";
-import TrackShowPageContainer from "../components/tracks/track_show_page_container";
+import TrackPlayerC from "../components/track_player/track_player_container";
+import TrackShowPageC from "../components/tracks/track_show_page_container";
 
 const App = () => (
   <>
     <Modal />
     {/* <ProtectedRoute path="/" component={NavBar} /> */}
     <Switch>
-      <ProtectedRoute path="/discover" component={TrackIndex} />
+      <Route path="/discover" component={TrackIndex} />
       <ProtectedRoute path="/upload" component={UploadPage} />
-      <ProtectedRoute path="/tracks/:trackId" component={TrackShowPageContainer} />
-      <AuthRoute path="/" component={SplashContainer} />
+      <Route path="/tracks/:trackId" component={TrackShowPageC} />
+      <AuthRoute path="/" component={SplashC} />
     </Switch>
-    {/* <TrackPlayerContainer /> */}
+    <TrackPlayerC />
   </>
 );
 
