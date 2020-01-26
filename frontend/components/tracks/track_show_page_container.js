c434e179c0e24661d4bd7f19d26a731375637215
Jaehyuk Lee <jhlumd@gmail.com>
2020-01-25 18:12:35 -0800

    Edit track show page container with comments

--- frontend/components/tracks/track_show_page_container.js
+++ frontend/components/tracks/track_show_page_container.js
@@ -1,30 +1,38 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import TrackShowPage from "./track_show_page";
 import {
   fetchTrack,
   deleteTrack,
   fetchCurrentTrack
 } from "../../actions/track_actions";
+import { fetchComments, clearComments } from "../../actions/comment_actions";
+import TrackShowPage from "./track_show_page";
 
 const mstp = (state, ownProps) => {
   const track = state.entities.tracks[ownProps.match.params.trackId] || null;
   const user = track ? state.entities.users[track.user_id] : null;
   const currentTrack = state.ui.currentTrack || null;
   const currentTime = state.ui.trackPlayer.currentTime || null;
+  const comments =
+    track && track.comments
+      ? track.comments.map(commentId => state.entities.comments[commentId])
+      : null;
   return {
     user,
     track,
     currentUserId: state.session.id,
     currentTrack,
-    currentTime
+    currentTime,
+    comments
   };
 };
 
 const mdtp = dispatch => ({
   fetchTrack: id => dispatch(fetchTrack(id)),
   deleteTrack: id => dispatch(deleteTrack(id)),
-  fetchCurrentTrack: id => dispatch(fetchCurrentTrack(id))
+  fetchCurrentTrack: id => dispatch(fetchCurrentTrack(id)),
+  fetchComments: trackId => dispatch(fetchComments(trackId)),
+  clearComments: () => dispatch(clearComments())
 });
 
 export default withRouter(connect(mstp, mdtp)(TrackShowPage));
